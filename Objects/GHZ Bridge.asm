eaf228dca732ff9e902d104fa4169b71175cb2cf
cvghivebrain <66194501+cvghivebrain@users.noreply.github.com>
2024-12-20 22:47:30 +0000

    Started another rewrite of GHZ bridge

--- Objects/GHZ Bridge.asm
+++ Objects/GHZ Bridge.asm
@@ -17,6 +17,7 @@ Bri_Index:	index *,,2
 
 		rsobj Bridge
 ost_bridge_y_start:	rs.w 1					; original y position
+ost_bridge_y_subspr:	rs.w 1					; y position of subsprites
 ost_bridge_bend:	rs.b 1					; number of pixels the bridge has been deflected
 ost_bridge_current_log:	rs.b 1					; log Sonic is currently standing on (left to right, starts at 0)
 ost_bridge_last_log:	rs.b 1					; log on the far right
@@ -25,59 +26,50 @@ ost_bridge_last_log:	rs.b 1					; log on the far right
 
 Bri_Main:	; Routine 0
 		addq.b	#2,ost_routine(a0)			; goto Bri_Solid next
-		move.l	#Map_Bri,ost_mappings(a0)
 		move.w	#tile_Kos_Bridge+tile_pal3,ost_tile(a0)
-		move.b	#render_rel,ost_render(a0)
+		move.b	#render_rel+render_sublvl,ost_render(a0)
 		move.w	#priority_2,ost_priority(a0)
-		move.b	#id_frame_bridge_blank,ost_frame(a0)
-		moveq	#0,d1
-		move.b	ost_subtype(a0),d1
-		andi.b	#$F,d1					; read low nybble of subtype
-		move.l	d1,d2
-		lsl.w	#3,d1					; multiply by 8
-		move.b	d1,ost_displaywidth(a0)
-		move.b	d1,ost_width(a0)
+		move.b	ost_subtype(a0),d3
+		andi.w	#$F,d3					; read low nybble of subtype
+		move.w	d3,d2
+		lsl.w	#3,d3					; multiply by 8
+		move.w	d3,ost_displaywidth_hi(a0)
+		move.w	d3,ost_width_hi(a0)
 		move.b	#8,ost_height(a0)
 		move.b	#StrId_Bridge,ost_name(a0)
-		move.w	ost_y_pos(a0),ost_bridge_y_start(a0)
+		move.w	ost_y_pos(a0),d4
+		move.w	d4,ost_bridge_y_start(a0)
+		subq.w	#8,d4
+		move.w	d4,ost_bridge_y_subspr(a0)
 		
-		sub.w	ost_x_pos(a0),d1
-		neg.w	d1					; d1 = x pos of left edge
-		addq.w	#8,d1
+		sub.w	ost_x_pos(a0),d3
+		neg.w	d3					; d3 = x pos of left edge
+		bsr.w	FindFreeSub				; find empty subsprite
+		bne.s	Bri_Solid				; branch if empty slot isn't found
+		move.w	d2,d0					; number of subsprites
 		subq.b	#1,d2
 		move.b	d2,ost_bridge_last_log(a0)		; save id of rightmost log
-		moveq	#0,d3
-		
-	.loop:
-		bsr.w	FindNextFreeObj
-		bne.s	Bri_Solid
-		move.l	#BridgeLog,ost_id(a1)			; load bridge log object
-		move.l	ost_mappings(a0),ost_mappings(a1)
-		move.w	ost_tile(a0),ost_tile(a1)
-		move.b	ost_render(a0),ost_render(a1)
-		move.b	#8,ost_displaywidth(a1)
-		move.b	#StrId_Bridge,ost_name(a1)
-		move.w	#priority_3,ost_priority(a1)
-		move.w	d1,ost_x_pos(a1)
-		addi.w	#16,d1					; next log will be 16px right
-		move.w	ost_y_pos(a0),ost_y_pos(a1)
-		move.b	d3,ost_subtype(a1)			; save log id
-		addq.b	#1,d3
-		saveparent
-		dbf	d2,.loop				; repeat for number of logs
+		moveq	#sprite2x2,d1				; size 2x2
+		move.w	ost_tile(a0),d2				; tile setting
+		moveq	#16,d5					; each log is 16px wide
+		moveq	#0,d6
+		bsr.w	InitSubXY
 
 Bri_Solid:	; Routine 2
 		shortcut
 		tst.w	(v_debug_active_hi).w
-		bne.w	DespawnFamily_NoDisplay			; branch if debug mode is in use
-		move.w	ost_bridge_y_start(a0),d0
-		bsr.s	Bri_Sink
+		bne.w	DespawnSub				; branch if debug mode is in use
 		bsr.w	SolidObjectTop_SkipChk
-		beq.w	DespawnFamily_NoDisplay			; branch if no collision
+		beq.s	.skip_solid				; branch if no collision
 		move.w	ost_solid_x_pos(a0),d4
 		lsr.w	#4,d4
 		move.b	d4,ost_bridge_current_log(a0)		; set current log based on Sonic's x pos on object
-		bra.w	DespawnFamily_NoDisplay
+		
+	.skip_solid:
+		getsonic					; a1 = OST of Sonic
+		move.w	ost_bridge_y_start(a0),d0
+		bsr.s	Bri_Sink
+		bra.w	DespawnSub
 		
 ; ---------------------------------------------------------------------------
 ; Subroutine to sink bridge when stood on
@@ -93,13 +85,18 @@ Bri_Sink:
 
 .standing_on:
 		cmpi.b	#$1E,ost_sink(a0)
-		beq.s	.update_y				; branch if at maximum sink level
+		bne.s	.keep_sinking				; branch if not at maximum sink level
+		tst.w	ost_x_vel(a1)
+		beq.s	.exit					; branch if Sonic isn't moving
+		bra.s	.update_y
+		
+	.keep_sinking:
 		addq.b	#2,ost_sink(a0)				; keep sinking
 
 .update_y:
 		moveq	#0,d1
 		move.b	ost_bridge_current_log(a0),d1
-		move.w	(v_ost_player+ost_x_pos).w,d2
+		move.w	ost_x_pos(a1),d2
 		cmp.w	ost_x_pos(a0),d2
 		bcs.s	.left_side				; branch if Sonic is on the left half
 		neg.b	d1
@@ -113,6 +110,8 @@ Bri_Sink:
 		
 	.default:
 		move.w	d0,ost_y_pos(a0)			; update position
+		
+	.exit:
 		rts
 		
 Bri_Sink_Data:
